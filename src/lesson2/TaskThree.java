dc8072dfcd11edc61a25b62d581e2d50f5f1890a
OlegKhudoerko <110164546+OlegKhudoerko@users.noreply.github.com>
2022-12-02 11:05:07 +0300

    Форматирование

--- src/lesson2/TaskThree.java
+++ src/lesson2/TaskThree.java
@@ -1,11 +1,34 @@
 package lesson2;
 
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskThree {
-    public static void main(String[] args) {
-
+    
+  
+    public static List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) return paths;
+        StringBuilder sb = new StringBuilder();
+        dfs(root, paths, sb);
+        return paths;
     }
 
+    private static void dfs(TreeNode root, List<String> ans, StringBuilder sb) {
+        if (root.left == null && root.right == null) {
+            sb.append(root.val);
+            ans.add(sb.toString());
+            return;
+        }
+        sb.append(root.val + "->");
+        if (root.left != null) {
+            dfs(root.left, ans, new StringBuilder(sb.toString()));
+        }
+        if (root.right != null) {
+            dfs(root.right, ans, new StringBuilder(sb.toString()));
+        }
+    }
 }
 
 
