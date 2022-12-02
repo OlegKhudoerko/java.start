857e83a00f2300d97f9b206e1abec1d9b5340295
OlegKhudoerko <110164546+OlegKhudoerko@users.noreply.github.com>
2022-12-02 11:06:50 +0300

    Форматирование

--- src/lesson2/TaskFour.java
+++ src/lesson2/TaskFour.java
@@ -1,8 +1,16 @@
 package lesson2;
 
-public class TaskFour {
-    public static void main(String[] args) {
+import javax.swing.tree.TreeNode;
 
+public class TaskFour {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
 }
